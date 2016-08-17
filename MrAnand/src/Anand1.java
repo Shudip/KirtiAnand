04886ad170b02026d91ca5ea7b952141d67a45f8
Shudip <shudipkumar_wagh@fulcrumww.com>
2016-08-17 15:47:10 +0530

    Update Anand1.java

--- MrAnand/src/Anand1.java
+++ MrAnand/src/Anand1.java
@@ -2,7 +2,7 @@
 public class Anand1 {
 
 	public static void main(String[] args) {
-	System.out.println("This is Mr. Anand Keerthi");
+	System.out.println("This is Mr. Thalaiva Anand Keerthi");
 
 	}
 
